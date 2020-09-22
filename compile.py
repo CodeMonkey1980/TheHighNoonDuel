b2a7f2a0de42715d9d417eebc785cd8c209c61dd
Sebastiaan Vreeken <sebastiaan@code20.nl>
2020-09-22 12:21:07 +0200

    Compile file

---
+++ compile.py
@@ -0,0 +1,21 @@
+import os
+
+
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need fine tuning.
+include_files = []
+packages = ['pygame']
+excludes = ['tkinter']
+
+# GUI applications require a different base on Windows (the default is for a
+# console application).
+base = None
+# if sys.platform == "win32":
+#     base = "Win32GUI"
+
+setup(name="The High Noon Duel",
+      version="0.1.0",
+      description="A reaction speed game set in the old old west.",
+      options={"build_exe": {'packages': packages, 'excludes': excludes, 'include_files': include_files}},
+      executables=[Executable("core.py", base=base)])
