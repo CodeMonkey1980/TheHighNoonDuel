688c7c694d5927b998470963c14c64e47f2c0952
Sebastiaan Vreeken <sebastiaan@code20.nl>
2020-09-27 14:39:33 +0200

    Moving version information to seperate file

--- compile.py
+++ compile.py
@@ -4,6 +4,8 @@ import os
 from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need fine tuning.
+import gameinfo
+
 include_files = ['resources/']
 packages = ['pygame']
 excludes = ['tkinter']
@@ -14,8 +16,8 @@ base = None
 # if sys.platform == "win32":
 #     base = "Win32GUI"
 
-setup(name="The High Noon Duel",
-      version="0.1.0",
-      description="A reaction speed game set in the old old west.",
+setup(name=gameinfo.name,
+      version=gameinfo.version,
+      description=gameinfo.description,
       options={"build_exe": {'packages': packages, 'excludes': excludes, 'include_files': include_files}},
       executables=[Executable("core.py", base=base, targetName='the-high-noon-duel')])
---
+++ gameinfo.py
@@ -0,0 +1,3 @@
+name = 'The High Noon Duel'
+version = '0.1.1'
+description = 'A reaction speed game set in the old old west.'
